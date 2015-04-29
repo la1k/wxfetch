7b542390ea0edafd6953be1cba3ad385424a53b8
Asgeir Bjørgan <asgeirbj@samfundet.no>
2015-04-29 20:00:11 +0200

    rectified QTH coordinate

--- main.cpp
+++ main.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <vector>
-#include "tracker.h"
 #include "receiver.h"
+#include "tracker.h"
 using namespace std;
 
-
-void wxsat_prepare(int *num_satellites, struct orbit*** satellites){
-	const char *tle_noaa15[2] = {"1 25338U 98030A   15111.44744815  .00000353  00000-0  16813-3 0  9990",
-	"2 25338  98.7719 110.0498 0010142 339.5996  20.4777 14.25603267880750"};
+//frequencies in kHz
+void wxsat_prepare(int *num_satellites, struct orbit*** satellites, float **freqs){
+	//const char *tle_noaa15[2] = {"1 25338U 98030A   15111.44744815  .00000353  00000-0  16813-3 0  9990",
+	//"2 25338  98.7719 110.0498 0010142 339.5996  20.4777 14.25603267880750"};
 	const char *tle_noaa18[2] = {"1 28654U 05018A   15111.52351205  .00000264  00000-0  16910-3 0  9993",
 	"2 28654  99.1819 102.7929 0015245 117.6325 242.6395 14.12181499511071"};
-	const char *tle_noaa19[2] = {"1 33591U 09005A   15111.47492914  .00000377  00000-0  23086-3 0  9993",
-	"2 33591  98.9850  61.2335 0013646 334.4839  25.5657 14.11916313319481"};
+	//const char *tle_noaa19[2] = {"1 33591U 09005A   15111.47492914  .00000377  00000-0  23086-3 0  9993",
+	//"2 33591  98.9850  61.2335 0013646 334.4839  25.5657 14.11916313319481"};
 
-	*num_satellites = 3;
+	*num_satellites = 1;
 	*satellites = new struct orbit*[*num_satellites];
 	(*satellites)[0] = new struct orbit;
-	(*satellites)[1] = new struct orbit;
-	(*satellites)[2] = new struct orbit;
-	orbit_init((*satellites)[0], tle_noaa15);
-	orbit_init((*satellites)[1], tle_noaa18);
-	orbit_init((*satellites)[2], tle_noaa19);
+	//(*satellites)[1] = new struct orbit;
+	//(*satellites)[2] = new struct orbit;
+	//orbit_init((*satellites)[0], tle_noaa15);
+	//orbit_init((*satellites)[1], tle_noaa18);
+	orbit_init((*satellites)[0], tle_noaa18);
+	//orbit_init((*satellites)[2], tle_noaa19);
+
+	*freqs = new float[*num_satellites];
+	(*freqs)[0] = 137.62*1.0e03;
+	//(*freqs)[1] = 137.9125*1.0e03;
+	//(*freqs)[2] = 137.1000*1.0e03;
 }
 
 
@@ -29,17 +35,22 @@ int main(){
 	//satellites
 	int num_satellites = 0;
 	struct orbit **satellites;
-	wxsat_prepare(&num_satellites, &satellites);
+	float *freqs;
+	wxsat_prepare(&num_satellites, &satellites, &freqs);
 	
 	//QTH
 	geodetic_t obs_geodetic;
-	obs_geodetic.lat = 41.716905*deg2rad;
-	obs_geodetic.lon = -72.727083*deg2rad;
+	obs_geodetic.lat = 63.422429*deg2rad;
+	obs_geodetic.lon = -10.395282*deg2rad;
 	obs_geodetic.alt = 25.0/1000.0;
 	obs_geodetic.theta = 0;
 
+	//RTL-SDR receiver
+	receiver_t receiver;
+	receiver_initialize(&receiver);
 
-	double time_step = 1.0f/(24.0f*60);
+
+	double satcalc_time_step = 1.0f/(24.0f*60);
 	double curr_time = 0;
 	const double DAYS_TO_SECONDS_FACTOR = 24*60*60;
 
@@ -49,24 +60,43 @@ int main(){
 		double time_of_arrival = 0;
 		int sat_ind = 0;
 		double time_of_departure = 0;
-		sattrack_get_best_elevation(curr_time, 1, time_step, &obs_geodetic, num_satellites, satellites, &sat_ind, &time_of_arrival, &time_of_departure);
+		sattrack_get_best_elevation(curr_time, 1, satcalc_time_step, &obs_geodetic, num_satellites, satellites, &sat_ind, &time_of_arrival, &time_of_departure);
 
 		double time_until_start_sec = (time_of_arrival - curr_time)*DAYS_TO_SECONDS_FACTOR;
 		double duration_secs = (time_of_departure - time_of_arrival)*DAYS_TO_SECONDS_FACTOR;
 		fprintf(stderr, "Calculated next satellite pass: cat %d in %f hours, lasting %f minutes.\n", satellites[sat_ind]->catnum, time_until_start_sec/(60*60), duration_secs/60);
 
+		double sattrack_timestep = 1.0f/(24.0f*60*60); //compensate for doppler shift every second
+		while (curr_time <= time_of_arrival){
+			curr_time = CurrentDaynum();
+			double azimuth, elevation;
+			sattrack_get_aziele(curr_time, &obs_geodetic, satellites[sat_ind], &azimuth, &elevation);
+			orbit_predict(satellites[sat_ind], curr_time);
+			//cout << Degrees(azimuth) << " " << Degrees(elevation) << endl;
+			//cout << Degrees(satellites[sat_ind]->latitude) << " " << 360-Degrees(satellites[sat_ind]->longitude) << endl;
+
+			sleep(1);
+		}
 
 		//sleep until approx. start
 		sleep(time_until_start_sec);
 
-		//satellite is here!
-		
-		sleep(duration_secs);
+		//satellite is here! Start gnuradio and doppler shift calculations. 
+		double sat_freqv = freqs[sat_ind];
+		receiver_set_frequency(&receiver, sat_freqv);
+		receiver_start(&receiver);
+		while (curr_time < time_of_departure){
+			curr_time = CurrentDaynum();
+			orbit_predict(satellites[sat_ind], curr_time);
 
+			double curr_freqv = sat_freqv*sattrack_get_doppler_shift(curr_time, satellites[sat_ind]);
+			receiver_set_frequency(&receiver, curr_freqv);
+		}
+		receiver_stop(&receiver);
+		
 		//sleep some extra time just to shake off the last satellite. 
 		sleep(60);
 	}
 	
 	return 0;
 }
-#endif
