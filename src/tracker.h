a828370ddb163f8e6cbeb25f92fa59fe79df3dbb
Asgeir Bjørgan <asgeirbj@samfundet.no>
2015-11-06 20:44:23 +0100

    Removed tracker code, since that is mostly within libpredict anyway.

--- src/tracker.h
+++
@@ -1,8 +0,0 @@
-
-extern "C"{
-#include <predict/orbit.h>
-}
-
-void sattrack_get_best_elevation(double time, double time_offset, double time_step, geodetic_t *qth_coord, int num_satellites, orbit **satellites, int *sat_ind, double *time_of_arrival, double *time_of_los);
-
-double sattrack_get_max_elevation(double start_time, double time_step, geodetic_t *qth_coord, orbit *satellite, double *ret_sat_duration);
