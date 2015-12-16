746799c528779acc85e934f99f0426f0af9fc3db
Asgeir Bjørgan <asgeirbj@samfundet.no>
2015-12-16 18:53:28 +0100

    Correction to initial channel offset.

--- wxdecode/src/image.cpp
+++ wxdecode/src/image.cpp
@@ -6,6 +6,6 @@ cv::Mat apt_image_channel(cv::Mat raw_image, int channel)
 		return cv::Mat();
 	}
 
-	return raw_image(cv::Rect(APT_SYNC_WIDTH + APT_TELE_WIDTH + channel*APT_CHANNEL_OFFSET, 0, APT_CHANNEL_WIDTH, raw_image.rows));
+	return raw_image(cv::Rect(APT_SYNC_WIDTH + APT_SPC_WIDTH + channel*APT_CHANNEL_OFFSET, 0, APT_CHANNEL_WIDTH, raw_image.rows));
 }
 
